1b2ebb0f094c23c9ff8e009fd56a280174a12bf9
Austin <austinjrosenbaum@gmail.com>
2018-02-27 13:54:36 -0700

    working on init_guess.vhd

--- Lab 4/init_guess.vhd
+++ Lab 4/init_guess.vhd
@@ -27,11 +27,34 @@ architecture init_guess_arch of init_guess is
   signal address_sig 	: std_logic_vector(7 downto 0);
   signal q_sig		: std_logic_vector(7 downto 0);
 
+  -- Holds the leading zeros of x
+  signal Z		: std_logic_vector(5 downto 0);
+
+  signal alpha		: signed(6 downto 0);
+  signal beta           : signed(6 downto 0);
+
+
   begin
 
-  rom_inst : rom PORT MAP (
-	     address	 => address_sig,
-             clock	 => clk,
-	     q		 => q_sig);
+  --rom_inst : rom PORT MAP (
+	--     address	 => address_sig,
+        --     clock	 => clk,
+	--     q		 => q_sig);
 
+  lzc_inst : lzc PORT MAP (clk => clk, lzc_vector => x, lzc_count => Z);
+
+  -- beta <= C - Z
+  beta <= (to_signed(C, 7) - signed(Z));
+  
+  ALPHA_PROC : process (clk)
+    begin
+      if(beta(0) = '0') then
+	alpha <= shift_right(beta, 1) - shift_left(beta, 1);
+      else
+	alpha <= shift_right((beta + "0000001"), 1) - shift_left(beta, 1);
+      end if;
+
+  end process;
+
+  y0 (6 downto 0) <= std_logic_vector(alpha);
 end architecture;
